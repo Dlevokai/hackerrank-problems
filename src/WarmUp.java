2df4f08fa4efbca2bd0d994201ccf049b7864eb2
Your Name <D.iakovel@gmail.com>
2016-10-22 16:13:18 -0400

    first commit

---
+++ src/WarmUp.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class WarmUp {
+	static int solveMeFirst(int a, int b) {
+		return a+b;
+	}
+
+	   
+	 public static void main(String[] args) {
+	        Scanner in = new Scanner(System.in);
+	        int sum=0;
+	        int n = in.nextInt();
+	        int arr[] = new int[n];
+	        for(int arr_i=0; arr_i < n; arr_i++){
+	            arr[arr_i] = in.nextInt();
+	        }
+	        sum = returnSumOfArray(n, arr);
+	        System.out.println(sum);
+	   }
+	 
+	 public static int returnSumOfArray(int length, int[] values){
+		 int sum=0;
+		 for (int value:values){
+			 sum+=value;
+		 }
+		 return sum;
+	 }
+}
